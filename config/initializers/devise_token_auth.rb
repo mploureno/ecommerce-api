b07a4dec24f8f82ba41f08d7cbcf8565b479d648
mp <mploureno@gmail.com>
2020-11-17 19:32:40 -0300

    configure devise to require token to change password

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -6,6 +6,9 @@ DeviseTokenAuth.setup do |config|
   # this to false to prevent the Authorization header from changing after
   # each request.
   config.change_headers_on_each_request = true
+  
+  # Require token to change password
+  config.require_client_password_reset_token = true
 
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
