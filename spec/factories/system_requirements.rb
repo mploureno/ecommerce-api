d6c44008f2aff6b5195d46fb42d347f996ab2782
mp <mploureno@gmail.com>
2020-11-22 18:40:28 -0300

    add faker to project

--- spec/factories/system_requirements.rb
+++ spec/factories/system_requirements.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :system_requirement do
-    name { "MyString" }
-    operational_system { "MyString" }
-    storage { "MyString" }
-    processor { "MyString" }
-    memory { "MyString" }
-    video_board { "MyString" }
+    sequence(:name) { |n| "System #{n}" }
+    operational_system { Faker::Computer.os }
+    storage { "500GB" }
+    processor { "AMD Ryzen 7" }
+    memory { "2GB" }
+    video_board { "RADEON" }
   end
 end
