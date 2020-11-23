a44eaa000d3313f53f47448674fd3079c354fb6f
mp <mploureno@gmail.com>
2020-11-22 23:16:51 -0300

    add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
+
+  validates :name, presence: :true, uniqueness: { case_sensitive: false }
+  validates :description, presence: :true
+  validates :price, presence: :true
 end
