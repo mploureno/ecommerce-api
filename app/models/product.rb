ddb2dce9cd54e752f8e22828f4124f4b8932b1a4
mp <mploureno@gmail.com>
2020-11-24 11:52:02 -0300

    associate with image and validate its presence

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
-
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
+  has_one_attached :image
 
   validates :name, presence: :true, uniqueness: { case_sensitive: false }
   validates :description, presence: :true
   validates :price, presence: :true
+  validates :image, presence: :true
 end
