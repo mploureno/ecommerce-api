c3fd41414e8e0d3accd6877144016372ef1d4b3e
mp <mploureno@gmail.com>
2020-12-04 20:35:51 -0300

    write specs for product availability status

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,11 +7,14 @@ RSpec.describe Product, type: :model do
   it { is_expected.to have_many(:product_categories).dependent(:destroy) }
   it { is_expected.to have_many(:categories).through(:product_categories) }
 
+  it {is_expected.to define_enum_for(:status).with_values({ available: 1, unavailable: 2 }) }
+
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:price) }
   it { is_expected.to validate_presence_of(:image) }
+  it { is_expected.to validate_presence_of(:status) }
 
   it_behaves_like "name searchable concern", :product
   it_behaves_like "paginatable concern", :product
